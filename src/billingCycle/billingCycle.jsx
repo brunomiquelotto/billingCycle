f2aa9dfe8b3854ccfa62350eaf8238f9d30e6d1c
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2017-07-02 23:41:40 -0300

    a

---
+++ src/billingCycle/billingCycle.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabHeader from '../common/tab/tabHeader'
+import TabsContent from '../common/tab/tabsContent'
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de pagamentos' small = 'Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList'/>
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate'/>
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate'/>
+                            <TabHeader label='Excluir' icon='trash' target='tabDelete'/>
+                        </TabsHeader>
+                        <TabsContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
